4f0f3abbcd9f0b8931c42f1b964c2903ef7b8b25
Adrien1126 <adriengluckman892@hotmail.fr>
2025-08-29 07:55:09 +0000

    Jour 1 : structure projet + hello_pricer.py

---
+++ hello_pricer.py
@@ -0,0 +1,14 @@
+def main():
+    art = r"""
+     ____  _             _               
+    |  _ \(_) ___  _ __ | |__   ___ _ __ 
+    | |_) | |/ _ \| '_ \| '_ \ / _ \ '__|
+    |  __/| | (_) | | | | | | |  __/ |   
+    |_|   |_|\___/|_| |_|_| |_|\___|_|   
+
+    """
+    print(art)
+    print("Hello, pricer world! Ready to crunch some swaps.")
+
+if __name__ == "__main__":
+    main()
